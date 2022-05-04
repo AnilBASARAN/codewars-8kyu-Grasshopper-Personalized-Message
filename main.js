8c2a73d82fff3940b336d39c60aa7f4c79b786ec
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-05 01:52:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+
+
+let greet = (name, owner) =>  name == owner ? "Hello boss" : "Hello guest"
+
+
+
+/*
+8kyu Grasshopper - Personalized Message
+
+
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/javascript
